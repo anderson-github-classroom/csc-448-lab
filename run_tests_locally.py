acf0e64cae2dfa602aa889275dbd9e7404a16859
Paul Anderson <pauleanderson@gmail.com>
2020-04-08 08:23:56 -0700

    update

--- run_tests_locally.py
+++ run_tests_locally.py
@@ -5,4 +5,4 @@ files = listdir('./')
 for file in files:
     if isfile(file) and file.endswith('.ipynb'):
         print("Testing %s" % file)
-        pytest.main(["../csc-448-student/tests/test_Syllabus.py"])
+        pytest.main(["../csc-448-student/tests/test_" + file[0:file.index('.ipynb')] + '.py'])
